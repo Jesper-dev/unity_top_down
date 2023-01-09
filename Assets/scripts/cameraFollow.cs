35b3dc9aab42ff2c506f85136f767a9e5d5c74ba
JesperKYH <jesper.pettersson@student.kyh.se>
2023-01-09 20:09:17 +0100

    Add colliders

--- Assets/scripts/cameraFollow.cs
+++ Assets/scripts/cameraFollow.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class cameraFollow : MonoBehaviour
 {
     public Transform followTransform;
-    void Start()
-    {
-        
-    }
-
+   
     void FixedUpdate()
     {
         this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
